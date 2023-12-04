1afaf03b31f7555ebaa383c988f05717c0edf5a5
SergonM <sa.gonzalezm123@uniandes.edu.co>
2023-12-03 20:07:32 -0500

    add

--- src/app/estado-reserva/estado-reserva.component.ts
+++ src/app/estado-reserva/estado-reserva.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Reserva } from '../reserva/reserva';
 
 @Component({
   selector: 'app-estado-reserva',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class EstadoReservaComponent implements OnInit {
 
   idIngresado: number = 0;
+  reserva!: Reserva;
 
   constructor() { }
 
